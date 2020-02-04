0acde13a81651a424eb2719bbea2403ede8d038d
Juan Mesaglio <jmesaglio@certant.com>
2020-02-04 14:37:44 -0300

    delete comments

--- api/controllers/hello_controller_v2.js
+++ api/controllers/hello_controller_v2.js
@@ -1,31 +1,9 @@
 'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
-/*
- Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
-​
- For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
-​
- Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
-​
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
- */
+
 module.exports = {
-  getHello: getHello
+    getHello: getHello
 };
 
-/*
-  Functions in a127 controllers used for operations should take two parameters:
-​
-  Param 1: a handle to the request object
-  Param 2: a handle to the response object
- */
 function getHello(req, res) {
-  // this sends back a JSON response which is a single string
-  res.json('Hello, Pika');
+    res.json('Hello, Pika');
 }
